cbc9a913b9824119df79505fe9baf551878dc2f6
ko2sist <47998605+ko2sist@users.noreply.github.com>
2023-07-28 17:32:27 +0900

    [Silver IV] Title: solved.ac, Time: 308 ms, Memory: 36240 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.java
+++ 백준/Silver/18110. solved．ac/solved．ac.java
@@ -8,20 +8,19 @@ public class Main {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int[] arr = new int[n];
         for(int i=0; i<n; i++) {
-        	pq.add(Integer.parseInt(br.readLine()));
+        	arr[i] = Integer.parseInt(br.readLine());
         }
         
         int sum = 0;
         int r = Math.round(0.15f*n);
-        for(int i=0; i<r; i++) {
-        	pq.poll();
+        Arrays.sort(arr);
+        
+        for(int i=r; i<n-r; i++) {
+        	sum+=arr[i];
         }
-        for(int i=0; i<n-2*r; i++) {
-        	sum += pq.poll();
-        }
-        	
+
         System.out.println(Math.round(sum/(double)(n-2*r)));
     }
 }
