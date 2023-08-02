5c23ed5900de69e2b4d68d0a099570e60a5f38df
ko2sist <47998605+ko2sist@users.noreply.github.com>
2023-08-02 20:43:23 +0900

    [Bronze V] Title: 등록, Time: 76 ms, Memory: 11220 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+// BJ #7287
+public class Main {
+	public static void main(String[] args) throws Exception{
+        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        // 
+        sb.append(144).append("\n").append("ko2sist");
+     
+        // 최종 결과 출력
+        System.out.println(sb);
+    }
+}
