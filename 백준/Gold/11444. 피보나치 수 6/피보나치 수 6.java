807ad1457730708e000998e5afad9d1e68cacfbe
ko2sist <47998605+ko2sist@users.noreply.github.com>
2023-08-23 09:22:21 +0900

    [Gold II] Title: 피보나치 수 6, Time: 92 ms, Memory: 11640 KB -BaekjoonHub

---
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+// BJ #11444 - 피보나치 수 6
+// Strategy: 도가뉴 항등식, 메모이제이션
+public class Main {
+	static Map<Long,Long> m;
+	static long denom = 1000000007L;
+	
+	public static long Fibo(long n) {
+		if(m.containsKey(n)) {
+			return m.get(n);
+		}
+		
+		if(n % 2 == 0) {
+			m.put(n/2, Fibo(n/2) % denom);
+			m.put(n/2-1, Fibo(n/2-1) % denom);
+			m.put(n/2+1, Fibo(n/2+1) % denom);
+			
+			return (Fibo(n/2) * (Fibo(n/2-1) + Fibo(n/2+1))) % denom;
+		}else {
+			m.put(n/2, Fibo(n/2) % denom);
+			m.put(n/2+1, Fibo(n/2+1) % denom);
+			
+			return (Fibo(n/2+1)*Fibo(n/2+1) + Fibo(n/2)*Fibo(n/2)) % denom;
+		}
+	}
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		long n = Long.parseLong(br.readLine());
+		m = new HashMap<>();
+		m.put(0L, 0L);
+		m.put(1L, 1L);
+		m.put(2L, 1L);
+		
+		System.out.println(Fibo(n));
+	}
+}
