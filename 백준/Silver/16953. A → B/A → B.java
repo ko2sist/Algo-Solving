69e9482231183aa919a94a03f3c058c043f9f9bc
ko2sist <47998605+ko2sist@users.noreply.github.com>
2023-08-23 10:30:09 +0900

    [Silver II] Title: A → B, Time: 80 ms, Memory: 11508 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.java
+++ 백준/Silver/16953. A → B/A → B.java
@@ -2,47 +2,8 @@ import java.util.*;
 import java.io.*;
 
 // BJ #16953 - A->B
-// Strategy: BFS
+// Strategy: 
 public class Main {
-	public static class Pair{
-		int num;
-		int op;
-		
-		public Pair(int num, int op) {
-			this.num = num;
-			this.op = op;
-		}
-	}
-	
-	public static int BFS(int A, int B) {
-		Queue<Pair> q = new LinkedList<>();
-		boolean[] visited = new boolean[1000000001];
-		
-		q.add(new Pair(A,1));
-		visited[A] = true;
-		
-		while(!q.isEmpty()) {
-			Pair tmp = q.poll();
-			long tn = tmp.num;
-			int to = tmp.op;
-			
-			if(tn * 2 == B || tn*10 + 1 == B) return to+1;
-			
-			if(tn * 2 < B && !visited[(int)tn*2]) {
-				q.add(new Pair((int)tn*2, to+1));
-				visited[(int)tn*2] = true;
-			}
-			
-			if(tn*10 + 1 < B && !visited[(int)tn*10+1]) {
-				q.add(new Pair((int)tn*10+1, to+1));
-				visited[(int)tn*10+1] = true;
-			}
-					
-		}
-		
-		return -1;
-	}
-	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 //		StringBuilder sb = new StringBuilder();
@@ -52,7 +13,27 @@ public class Main {
 		int A = Integer.parseInt(st.nextToken());
 		int B = Integer.parseInt(st.nextToken());
 		
+		int res = 1;
 		
-		System.out.println(BFS(A,B));
+		while(true) {
+			if(A == B) break;
+			if(B < A) {
+				res = -1;
+				break;
+			}
+			
+			if(B%10 == 1) {
+				B = (B-1) / 10;
+				res++;
+			}else if(B%2 == 0) {
+				B = B / 2;
+				res++;
+			}else {
+				res = -1;
+				break;
+			}
+		}
+		
+		System.out.println(res);
 	}
 }
