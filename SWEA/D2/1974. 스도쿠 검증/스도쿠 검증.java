efecef5a9add43f849ca172d8593bf5c4228a768
ko2sist <47998605+ko2sist@users.noreply.github.com>
2023-08-24 12:03:36 +0900

    [D2] Title: 스도쿠 검증, Time: 147 ms, Memory: 23,856 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
@@ -4,21 +4,21 @@ import java.io.*;
 // SWEA #1974 - 스도쿠 검증
 // Strategy:  
 class Solution {
-    public static int sudoku(int[][] sudoku_array){
+    public static int check(int[][] sudoku_array){
         int val = 1;
-        Set<Integer> check_row = new HashSet<Integer>();
-        Set<Integer> check_col = new HashSet<Integer>();
-        Set<Integer> check_box = new HashSet<Integer>();
+        Set<Integer> check_row = new HashSet<>();	// 가로 체크를 위한 Set
+        Set<Integer> check_col = new HashSet<>();	// 세로 체크를 위한 Set
+        Set<Integer> check_box = new HashSet<>();	// 박스 체크를 위한 Set
 
         loop1: for (int i = 0; i<9; i++){
-            // Adding
+            // 스도쿠 set에 저장
             for (int j = 0; j<9; j++){
                 check_row.add(sudoku_array[i][j]);
                 check_col.add(sudoku_array[j][i]);
                 check_box.add(sudoku_array[(i/3)*3+j/3][(i%3)*3+j%3]);
             }
 
-            // Check Validation
+            // 유효성 검증
             if (check_row.size() != 9){
                 val = 0;
                 break loop1;
@@ -39,18 +39,30 @@ class Solution {
 
 
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T;
-        T = sc.nextInt();
+    public static void main(String[] args) throws Exception{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+        
+		// T: 테스트케이스 수
+        int T = Integer.parseInt(br.readLine());
 
         for (int t=1; t<T+1; t++){
-            int[][] temp_sudoku = new int[9][9];
-            // Get Sudoku
-            for(int i=0; i<81; i++){
-                temp_sudoku[i/9][i%9] = sc.nextInt();
+        	// sudoku: 스도쿠를 저장할 2차원 배열
+            int[][] sudoku = new int[9][9];
+            
+            // 스도쿠 입력 받기
+            for(int i=0; i<9; i++){
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for(int j=0; j<9; j++) {
+                	sudoku[i][j] = Integer.parseInt(st.nextToken());
+                }
             }
-            System.out.println("#"+t+" "+sudoku(temp_sudoku));
+            
+            // 현재 테스트 케이스 결과 저장
+            sb.append("#"+t).append(" ").append(check(sudoku)).append("\n");
         }
+        
+        // 최종 결과 출력
+        System.out.println(sb);
     }
 }
