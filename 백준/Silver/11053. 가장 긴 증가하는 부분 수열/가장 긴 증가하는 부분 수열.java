0c5cc5773faad233b16f8be28f6e4c4d11134d38
ko2sist <47998605+ko2sist@users.noreply.github.com>
2023-08-27 19:17:55 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 84 ms, Memory: 11784 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 // BJ #11053 - 가장 긴 증가하는 부분 수열
-// Strategy: DP
+// Strategy: LIS + Binary Search
 public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -10,31 +10,39 @@ public class Main {
 		
 		int N = Integer.parseInt(br.readLine());
 		
-		int[] nums = new int[N];
+		int[] lis = new int[N];
+		int len = 0;
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++) {
-			nums[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		int[] len = new int[N];
-		for(int i=0; i<N; i++) {
-			len[i] = 1;
-		}
-		
-		int max = 1;
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<i; j++) {
-				if(nums[j] < nums[i]) {
-					len[i] = Math.max(len[j]+1, len[i]);
-					if(len[i] > max) {
-						max = len[i];
+			int tmp = Integer.parseInt(st.nextToken());
+			
+			if(i == 0) {
+				lis[0] = tmp;
+				len++;
+			}else {
+				if(lis[len-1] < tmp) {
+					lis[len] = tmp;
+					len++;
+				}else {
+					int start = 0;
+					int end = len-1;
+					while(start <= end) {
+						int mid = (start+end) / 2;
+						if(tmp > lis[mid]) {
+							start = mid+1;
+						}else if(tmp < lis[mid]){
+							end = mid-1;
+						}else {
+							start = mid;
+							break;
+						}
 					}
+					lis[start] = tmp;
 				}
 			}
 		}
-		
 
 		// 최종 결과 출력
-		System.out.print(max);
+		System.out.println(len);
 	}
 }
