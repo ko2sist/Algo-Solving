f293c573e7068c804150adc03cc497e1ac8f63a1
ko2sist <47998605+ko2sist@users.noreply.github.com>
2023-08-31 11:36:43 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 100 ms, Memory: 19,148 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
@@ -2,32 +2,34 @@ import java.util.*;
 import java.io.*;
 
 // SWEA #1217 - 거듭제곱
-class Solution {
+// Strategy: 
+public class Solution {
 	public static int pow(int n, int p) {
-		if(p == 1) {
-			return n;
+		if(p == 0) return 1;
+		if(p == 1) return n;
+		
+		int tmp = pow(n, p/2);
+		if(p%2 == 0) {
+			return tmp*tmp;
 		}else {
-			return n*pow(n,p-1);
+			return tmp*tmp*n;
 		}
 	}
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		
-		// T: testcase 개수
+		// T: 테스트 케이스 수
 		int T = 10;
 		
 		for(int t=1; t<=T; t++) {
 			int tc = Integer.parseInt(br.readLine());
 			
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			int P = Integer.parseInt(st.nextToken());
+			int n = Integer.parseInt(st.nextToken());
+			int p = Integer.parseInt(st.nextToken());
 			
-			int res = pow(N,P);
-			
-			// 결과 저장
-			sb.append("#"+t).append(" ").append(res).append("\n");
+			sb.append("#").append(t).append(" ").append(pow(n,p)).append("\n");
 		}
 		// 최종 결과 출력
 		System.out.println(sb);
