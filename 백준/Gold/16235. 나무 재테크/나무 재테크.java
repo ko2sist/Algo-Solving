c03a545c87a878e46408e1f3bcc0d07c3c811b85
ko2sist <47998605+ko2sist@users.noreply.github.com>
2023-09-01 06:04:59 +0900

    [Gold III] Title: 나무 재테크, Time: 480 ms, Memory: 162052 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.java
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.java
@@ -2,78 +2,71 @@ import java.util.*;
 import java.io.*;
 
 // BJ #16235 - 나무 재테크
-// Strategy: PQ를 이용해서 나무의 나이 순으로 정렬, 양분 흡수 관리
+// Strategy: deque
 public class Main {
 	static int N, M, K;
 	static int[][] A, nutrient;
-	static Trees[][] land;
+	static ArrayDeque<Tree> trees;
+	static ArrayDeque<Tree> dead;
 	static int[] dr = {-1,-1,-1,0,0,1,1,1};
 	static int[] dc = {-1,0,1,-1,1,-1,0,1};
 	
-	public static class Trees{
-		PriorityQueue<Integer> ages;
-		PriorityQueue<Integer> dead;
+	public static class Tree{
+		int row;
+		int col;
+		int age;
 		
-		public Trees() {
-			ages = new PriorityQueue<>();
-			dead = new PriorityQueue<>();
+		public Tree(int row, int col, int age) {
+			this.row = row;
+			this.col = col;
+			this.age = age;
 		}
 	}
 	
 	public static void Spring() {
-		for(int i=1; i<=N; i++) {
-			for(int j=1; j<=N; j++) {
-				PriorityQueue<Integer> pq = new PriorityQueue<>();
-				PriorityQueue<Integer> ages = land[i][j].ages;
-				PriorityQueue<Integer> dead = land[i][j].dead;
-				while(!ages.isEmpty()) {
-					int tmp_age = ages.poll();
-					if(tmp_age <= nutrient[i][j]) {
-						pq.add(tmp_age+1);
-						nutrient[i][j] -= tmp_age;
-					}else {
-						dead.add(tmp_age);
-					}
-				}
-				land[i][j].ages = pq;
+		int size = trees.size();
+		
+		while(size-- > 0) {
+			Tree tmp = trees.pollFirst();
+			
+			if(tmp.age <= nutrient[tmp.row][tmp.col]) {
+				nutrient[tmp.row][tmp.col] -= tmp.age;
+				tmp.age++;
+				trees.add(tmp);
+			}else {
+				dead.add(tmp);
 			}
 		}
 	}
 	
 	public static void Summer() {
-		for(int i=1; i<=N; i++) {
-			for(int j=1; j<=N; j++) {
-				if(land[i][j].ages.size() != 0) {
-					PriorityQueue<Integer> pq = new PriorityQueue<>();
-					PriorityQueue<Integer> ages = land[i][j].ages;
-					
-					while(!ages.isEmpty()) {
-						int tmp = ages.poll();
-						pq.add(tmp);
-						if(tmp % 5 == 0) {
-							for(int k=0; k<8; k++) {
-								int nr = i+dr[k];
-								int nc = j+dc[k];
-								if(nr <= 0 || nc <= 0 || nr > N || nc > N) continue;
-								
-								land[nr][nc].ages.add(1);
-							}
-						}
-					}
-					land[i][j].ages = pq;
-				}
-			}
+		while(!dead.isEmpty()) {
+			Tree tmp = dead.poll();
+			nutrient[tmp.row][tmp.col] += tmp.age/2;
 		}
 	}
 	
 	public static void Fall() {
-		for(int i=1; i<=N; i++) {
-			for(int j=1; j<=N; j++) {
-				PriorityQueue<Integer> dead = land[i][j].dead;
-				while(!dead.isEmpty()) {
-					int tmp = dead.poll();
-					nutrient[i][j] += tmp/2;
-				}
+		ArrayDeque<Tree> dq = new ArrayDeque<>();
+		int size = trees.size();
+		
+		while(size-- > 0) {
+			Tree tmp = trees.pollFirst();
+			
+			if(tmp.age % 5 == 0) {
+				dq.add(tmp);
+			}
+			trees.add(tmp);
+		}
+		
+		while(!dq.isEmpty()) {
+			Tree tmp = dq.poll();
+			
+			for(int i=0; i<8; i++) {
+				int nr = tmp.row + dr[i];
+				int nc = tmp.col + dc[i];
+				if(nr <= 0 || nc <= 0 || nr > N || nc > N) continue;
+				trees.addFirst(new Tree(nr,nc,1));
 			}
 		}
 	}
@@ -92,17 +85,8 @@ public class Main {
 			Summer();
 			Fall();
 			Winter();
+			
 		}
-	}
-	
-	public static int getNumTrees() {
-		int res = 0;
-		for(int i=1; i<=N; i++) {
-			for(int j=1; j<=N; j++) {
-				res += land[i][j].ages.size();
-			}
-		}
-		return res;
 	}
 	
     public static void main(String[] args) throws Exception {
@@ -121,8 +105,9 @@ public class Main {
         // nutrient: 땅의 각 칸이 현재 가지고 있는 양분의 정보를 저장하는 배열
         nutrient = new int[N+1][N+1];
         
-        // land: 땅의 각 칸에 심어진 나무의 나이를 저장하는 Trees class를 저장하는 배열
-        land = new Trees[N+1][N+1];
+        // 
+        trees = new ArrayDeque<>();
+        dead = new ArrayDeque<>();
         
         // A, nutrient 초기 정보 입력 및 land 초기화
         for(int i=1; i<=N; i++) {
@@ -130,22 +115,21 @@ public class Main {
             for(int j=1; j<=N; j++) {
                 A[i][j] = Integer.parseInt(st.nextToken());
                 nutrient[i][j] = 5;
-                land[i][j] = new Trees();
             }
         }
         
         // 초기에 심어진 나무의 정보 입력
         for(int i=0; i<M; i++) {
             st = new StringTokenizer(br.readLine());
-            int r = Integer.parseInt(st.nextToken());    // r: 나무 위치
-            int c = Integer.parseInt(st.nextToken());    // c: 나무 위치
-            int age = Integer.parseInt(st.nextToken());    // age: 나무의 나이
+            int x = Integer.parseInt(st.nextToken());    // r: 나무 위치
+            int y = Integer.parseInt(st.nextToken());    // c: 나무 위치
+            int z = Integer.parseInt(st.nextToken());    // age: 나무의 나이
             
-            land[r][c].ages.add(age);
+            trees.add(new Tree(x,y,z));
         }
         
         Invest();
         
-        System.out.println(getNumTrees());
+        System.out.println(trees.size());
     }
 }
