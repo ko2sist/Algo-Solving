700ff2b05afe8e093b93079d474e75701d928ff3
ko2sist <47998605+ko2sist@users.noreply.github.com>
2023-09-10 03:29:12 +0900

    [Bronze V] Title: The World Responds, Time: 68 ms, Memory: 11444 KB -BaekjoonHub

---
+++ 백준/Bronze/23234. The World Responds/The World Responds.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+// BJ 
+// Strategy: 
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("The world says hello!");
+	}
+}
