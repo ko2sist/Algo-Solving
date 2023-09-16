627eec603557084edadb8d4972d2a39186e7897c
ko2sist <47998605+ko2sist@users.noreply.github.com>
2023-09-17 00:05:16 +0900

    [Bronze V] Title: 연세대학교, Time: 76 ms, Memory: 11288 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+import java.math.BigInteger;
+
+// BJ #
+// Strategy:
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		if(n == 0) {
+			System.out.println("YONSEI");
+		}else {
+			System.out.println("Leading the Way to the Future");
+		}
+	}
+}
