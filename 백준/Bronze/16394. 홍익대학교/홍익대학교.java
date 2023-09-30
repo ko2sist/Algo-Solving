88fe9746f0c3dfc3c785fea317888105c7cad373
ko2sist <47998605+ko2sist@users.noreply.github.com>
2023-09-30 21:32:59 +0900

    [Bronze V] Title: 홍익대학교, Time: 76 ms, Memory: 11460 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+// BJ #16394 - 홍익대학교
+// Strategy: 
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        System.out.println(N-1946);
+    }
+}
