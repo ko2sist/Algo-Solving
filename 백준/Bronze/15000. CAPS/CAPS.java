7a13f5f816fc15f967f75a901dfa37dc7e5f0c15
ko2sist <47998605+ko2sist@users.noreply.github.com>
2023-10-02 22:31:11 +0900

    [Bronze V] Title: CAPS, Time: 224 ms, Memory: 23656 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+// BJ #
+// Strategy: 구현
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        System.out.println(br.readLine().toUpperCase());
+    }
+}
