5c5a58964c2c879aeb966a4c0f4c88ddfdb05260
ko2sist <47998605+ko2sist@users.noreply.github.com>
2023-10-11 21:12:54 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 68 ms, Memory: 11268 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+// BJ #
+// Strategy: 
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+       
+        System.out.println(2023);
+        System.out.println(10);
+        System.out.println(11);
+	}
+}
