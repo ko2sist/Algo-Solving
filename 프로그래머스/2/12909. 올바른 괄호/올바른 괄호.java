093ae6028efc1a737e16dea398145c8e43148846
ko2sist <47998605+ko2sist@users.noreply.github.com>
2023-10-21 18:58:19 +0900

    [level 2] Title: 올바른 괄호, Time: 15.94 ms, Memory: 62.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            
+            if(c == '('){
+                stack.push(c);
+            }else{
+                if(stack.isEmpty() || stack.pop() != '('){
+                    return false;
+                }
+            }
+        }
+        
+        if(stack.isEmpty()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
