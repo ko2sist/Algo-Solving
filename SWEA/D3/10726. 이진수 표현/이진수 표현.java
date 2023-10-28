e5d87b8b3cc5c373b805c45726f873ee12746656
ko2sist <47998605+ko2sist@users.noreply.github.com>
2023-10-28 11:14:33 +0900

    [D3] Title: 이진수 표현, Time: 150 ms, Memory: 28,560 KB -BaekjoonHub

--- SWEA/D3/10726. 이진수 표현/이진수 표현.java
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.java
@@ -15,12 +15,12 @@ public class Solution {
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
 			
-			String res = "ON";
-			for(int i=0; i<N; i++) {
-				if((M & (1<<i)) != (1<<i)) {
-					res = "OFF";
-					break;
-				}
+			String res="";
+			int bit = (1<<N)-1;
+			if((M & bit) == bit) {
+				res = "ON";
+			}else {
+				res = "OFF";
 			}
 			
 			sb.append("#"+t+" ").append(res).append("\n");
