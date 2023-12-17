4b3fe0a31379e2cb0a75b8ead8241578639480b4
ko2sist <47998605+ko2sist@users.noreply.github.com>
2023-12-18 05:59:17 +0900

    [Bronze V] Title: 나는 누구인가, Time: 68 ms, Memory: 11452 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+// BJ #1427 - 소트인사이드
+// Strategy: 
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+       
+        System.out.println("I'm Sexy");
+	}
+}
