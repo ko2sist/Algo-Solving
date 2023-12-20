b4990c3e09d6ad308a4da579e0cb2c9f574ed738
ko2sist <47998605+ko2sist@users.noreply.github.com>
2023-12-21 02:00:42 +0900

    [Bronze V] Title: 웰컴, Time: 68 ms, Memory: 11268 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+
+// BJ #
+public class Main {
+	static int N;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		System.out.println(".  .   . \n" + 
+				"|  | _ | _. _ ._ _  _ \n" + 
+				"|/\\|(/.|(_.(_)[ | )(/.");
+	}
+}
