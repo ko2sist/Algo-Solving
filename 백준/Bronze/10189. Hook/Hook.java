42fd07a0d047556c97a3c133ac6a34cca6657b48
ko2sist <47998605+ko2sist@users.noreply.github.com>
2023-12-28 00:19:23 +0900

    [Bronze V] Title: Hook, Time: 68 ms, Memory: 11268 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+// BJ #10189 - Hook
+// Strategy: 
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		System.out.println("#  # #### #### #  #\n"
+				+ "#### #  # #  # # #\n"
+				+ "#### #  # #  # # #\n"
+				+ "#  # #### #### #  #");
+	}
+
+}
